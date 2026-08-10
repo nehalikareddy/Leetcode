d800ba4cf1db8e0edda84288f3d0d5b364098143
Nadikatla Nehalika <nehalikareddy@gmail.com>
2026-08-10 11:48:02 +0530

    Time: 160 ms (5.47%), Space: 72.8 MB (94.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,12 +3,12 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
+            pq.offer(nums[i]);
         }
 
         int el = Integer.MAX_VALUE;
         for(int i=0;i<k;i++){
-            el = Math.min(pq.remove(),el);
+            el = Math.min(pq.poll(),el);
         }
         return el;
 
